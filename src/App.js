cbdd1f61fcde33b79a4a0d8a0e82dee2ff7d9c44
Marcos Taron <marcosvtd@gmail.com>
2020-04-26 20:21:58 -0300

    Add logic for add and remove repository

--- src/App.js
+++ src/App.js
@@ -1,29 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repos, setRepos] = useState([]);
+  const [formData, setFormData] = useState({ url: "", title: "", techs: "" });
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepos(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository(e) {
+    try {
+      e.preventDefault();
+
+      const { data } = await api.post("/repositories", {
+        ...formData,
+        techs: formData.techs.split(",").map((item) => item.trim()),
+      });
+
+      setFormData({ url: "", title: "", techs: "" });
+      setRepos([...repos, data]);
+    } catch (e) {
+      alert("Algo deu errado, verifique os dados e tente novamente.");
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepos(repos.filter((repos) => repos.id !== id));
+    } catch (e) {
+      alert("Algo deu errado, tente novamente");
+    }
   }
 
   return (
-    <div>
+    <div className="main">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map(({ id, title, techs, url }) => (
+          <li key={id}>
+            <div className="repository-info">
+              <h3>{title}</h3>
+              <span>
+                {techs.map((tech, index) => (index === 0 ? tech : `, ${tech}`))}
+              </span>
+              <a href={url}>> acessar</a>
+            </div>
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form>
+        <div className="inputGroup">
+          <label>URL do repositório</label>
+          <input
+            name="url"
+            type="text"
+            value={formData.url}
+            onChange={(e) => setFormData({ ...formData, url: e.target.value })}
+          />
+        </div>
+        <div className="inputGroup">
+          <label>Nome do repositório</label>
+          <input
+            name="title"
+            type="text"
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+          />
+        </div>
+        <div className="inputGroup">
+          <label>Tecnologias (separado por vírgula)</label>
+          <input
+            name="techs"
+            type="text"
+            value={formData.techs}
+            onChange={(e) =>
+              setFormData({ ...formData, techs: e.target.value })
+            }
+          />
+        </div>
+        <button onClick={(e) => handleAddRepository(e)}>Adicionar</button>
+      </form>
     </div>
   );
 }
